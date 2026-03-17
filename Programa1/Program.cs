74436ab6322d5d797db93e54509c2e09b3167f89
ALUMNO <ALUMNO@DESKTOP-A34TM46>
2026-03-17 08:11:14 -0600

    Agregar archivos de proyecto.

---
+++ Programa1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
+
+
+class Estudiante
+{
+    public string nombre;
+    public double nota1;
+    public double nota2;
+    public double nota3;
+
+
+    public double CalcularPromedio()
+    {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+    public string Aprobacion()
+    {
+        if (CalcularPromedio() >= 61)
+        {
+            return "Aprobado";
+        }
+        else
+        {
+            return "Reprobado";
+        }
+    }
+
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Nombre:  {nombre}  | Promedio:   {CalcularPromedio():F2}  | :  {Aprobacion():F2}");
+    }
+
+}
+
