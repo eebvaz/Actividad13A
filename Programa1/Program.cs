cf6f7dc47275a0c81bdb90af7c06839d1346dd16
ALUMNO <ALUMNO@DESKTOP-A34TM46>
2026-03-17 08:26:08 -0600

    Primero terminado

--- Programa1/Program.cs
+++ Programa1/Program.cs
@@ -10,9 +10,55 @@ namespace Programa1
     {
         static void Main(string[] args)
         {
+            List<Estudiante> estudiante = new List<Estudiante>();
+            int cant;
 
 
+            Console.WriteLine("Ingrese la cantidad de estudiantes a añadir: ");
+            cant = int.Parse(Console.ReadLine());
+            Console.Clear();
+            for (int i = 0; i <= cant; i++)
+            {
+                Console.Clear();
 
+                Estudiante alumno = new Estudiante();
+
+                Console.WriteLine("Nombre del alumno: ");
+                alumno.nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese la nota1: ");
+                alumno.nota1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la nota2: ");
+                alumno.nota2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la nota3: ");
+                alumno.nota3 = double.Parse(Console.ReadLine());
+                estudiante.Add(alumno);
+
+            }
+
+            Console.Clear();
+            Console.ReadKey();
+
+            double sumatoria = 0;
+            Estudiante muesta = estudiante[0];
+            Console.WriteLine("LISTA DE ESTUDIANTES");
+            foreach (Estudiante item in estudiante)
+            {
+                item.MostrarDatos();
+
+                sumatoria = sumatoria + item.CalcularPromedio();
+
+                if (item.CalcularPromedio() > muesta.CalcularPromedio())
+                {
+                    muesta = item;
+                }
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+
+            Console.WriteLine("Promedio de los alumnos: " + sumatoria / estudiante.Count);
+            Console.WriteLine("Mejor resultado: ");
+            muesta.MostrarDatos();
         }
     }
 }
@@ -44,7 +90,7 @@ class Estudiante
 
     public void MostrarDatos()
     {
-        Console.WriteLine($"Nombre:  {nombre}  | Promedio:   {CalcularPromedio():F2}  | :  {Aprobacion():F2}");
+        Console.WriteLine($"Nombre:  {nombre}  | Promedio:   {CalcularPromedio():F2}  | Estado:  {Aprobacion():F2}");
     }
 
 }
