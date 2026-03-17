bb3144096b167fe88a4a702c126a444ca51eb2b2
ALUMNO <ALUMNO@DESKTOP-A34TM46>
2026-03-17 09:21:15 -0600

    falta terminar

---
+++ Programa3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
+
+
+class Empleado
+{
+    public string nombre;
+    public string puesto;
+    public double salario;
+
+
+    public double Sueldo()
+    {
+            return salario * 12;
+    }
+
+    public double Bono()
+    {
+
+
+        return (salario * 12) / 12;
+
+    }
+
+    public string Clasificacion()
+    {
+        string resultado = "";
+
+
+
+        return resultado;
+
+    }
+
+
+
+}
