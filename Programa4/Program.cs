c7fcd25a31f6f8af1273185e407bd910866f0e40
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-18 17:11:10 -0600

    Numero 4 listo

--- Programa4/Program.cs
+++ Programa4/Program.cs
@@ -10,6 +10,81 @@ namespace Programa4
     {
         static void Main(string[] args)
         {
+
+            List<Libro> libros = new List<Libro>();
+
+            int cant;
+            Console.WriteLine("Cantidad de libros:");
+            cant = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            for (int i = 1; i <= cant; i++)
+                {
+
+                Libro cuadernos = new Libro();
+
+                Console.WriteLine("Titulo:  ");
+                cuadernos.titulo = Console.ReadLine();
+
+                Console.WriteLine("Autor:  ");
+                cuadernos.autor = Console.ReadLine();
+
+                Console.WriteLine("Categoria:   ");
+                cuadernos.categoria = Console.ReadLine();
+
+                Console.WriteLine("Paginas:  ");
+                cuadernos.paginas = int.Parse(Console.ReadLine());
+
+                libros.Add(cuadernos);
+                Console.Clear();
+
+
+            }
+
+            Libro mayor = libros[0];
+
+            foreach (Libro item in libros)
+            {
+                item.Mostrar();
+
+                if (item.paginas > mayor.paginas)
+                {
+                    mayor = item;
+                }
+            }
+
+            Console.WriteLine("Libro con mayor cantidad de paginas:   ");
+            mayor.Mostrar();
+
+
         }
+    }
+}
+class Libro
+{
+    public string titulo;
+    public string autor;
+    public string categoria;
+    public int paginas;
+
+    public string Cantidad()
+    {
+        if (paginas < 100)
+        {
+            return "Corto";
+        }
+        else if (paginas <= 300)
+        {
+            return "Mediano";
+        }
+        else
+        {
+            return "Extenso";
+        }
+    }
+
+    public void Mostrar()
+    {
+        Console.WriteLine($"Titulo: {titulo} | Autor: {autor} | Categoria: {categoria} | Paginas: {paginas} | Tipo: {Cantidad()}");
     }
 }
