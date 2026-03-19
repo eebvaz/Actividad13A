465593f94fbb83ee295567ba4bdc94c71a0eea0f
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-18 19:31:14 -0600

    Octavo listo

--- Programa8/Program.cs
+++ Programa8/Program.cs
@@ -10,6 +10,89 @@ namespace Programa8
     {
         static void Main(string[] args)
         {
+
+            List<Cuenta> cuentas = new List<Cuenta>();
+
+            int cant;
+
+            Console.WriteLine("Ingrese la cantidad de cuentas:");
+            cant = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            for (int i = 1; i <= cant; i++)
+            {
+                Cuenta cuentaBancaria = new Cuenta();
+                Console.Clear();
+                Console.WriteLine("Titular:");
+                cuentaBancaria.titular = Console.ReadLine();
+
+                Console.WriteLine("Saldo inicial:");
+                cuentaBancaria.saldo = double.Parse(Console.ReadLine());
+
+                cuentas.Add(cuentaBancaria);
+
+            }
+
+            Console.WriteLine("");
+            Console.ReadKey();
+            Console.Clear();
+            foreach (Cuenta item in cuentas)
+            {
+                double depositar1, retirar1;
+
+                int opcion=0;
+
+                Console.WriteLine("Ingrese su opcion: ");
+                Console.WriteLine("Opcion 1; depositar");
+                Console.WriteLine("Opcion 2: retirar");
+                opcion = int.Parse(Console.ReadLine());
+
+                if (opcion == 1)
+                {
+                    Console.Write("Deposito: Q");
+                    depositar1 = double.Parse(Console.ReadLine());
+                    item.Depositar(depositar1);
+
+                }
+                else
+                {
+                    Console.Write("Retiro: Q");
+                    retirar1 = double.Parse(Console.ReadLine());
+                    item.Retirar(retirar1);
+
+                }
+
+
+       
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            foreach (Cuenta item in cuentas)
+            {
+                item.MostrarDatos();
+            }
+
         }
+    }
+}
+class Cuenta
+{
+    public string titular;
+    public double saldo;
+
+    public void Depositar(double monto)
+    {
+        saldo += monto;
+    }
+
+    public void Retirar(double monto)
+    {
+        saldo -= monto;
+    }
+
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Titular: {titular} | Saldo final: {saldo}");
     }
 }
